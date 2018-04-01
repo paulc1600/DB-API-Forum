08604c3898dced93b858aecd43eddfc75e4df778
paulc1600 <ctmtest2304@gmail.com>
2018-03-31 21:10:12 -0700

    Add db API code that fetches and stores posts

--- forumdb.py
+++ forumdb.py
@@ -1,15 +1,20 @@
 # "Database code" for the DB Forum.
 
+import psycopg2
 import datetime
-
-POSTS = [("This is the first post.", datetime.datetime.now())]
 
 def get_posts():
   """Return all posts from the 'database', most recent first."""
-  return reversed(POSTS)
+  conn = psycopg2.connect("dbname=forum")
+  cursor = conn.cursor()
+  cursor.execute("select content, time from posts by time desc")
+  return = cursor.fetchall()
+  conn.close()
 
 def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
-  POSTS.append((content, datetime.datetime.now()))
-
-
+  conn = psycopg2.connect("dbname=forum")
+  cursor = conn.cursor()
+  cursor.execute("insert into posts values ('%s')" % content)
+  conn.commit()
+  conn.close()
