2f8e62dc1adf420827101b78be7926bb16f77d10
Paul <35120590+paulc1600@users.noreply.github.com>
2018-04-04 20:46:59 -0700

    3rd time charm for closing before return

--- forumdb.py
+++ forumdb.py
@@ -8,8 +8,9 @@ def get_posts():
   conn = psycopg2.connect("dbname=forum")
   cursor = conn.cursor()
   cursor.execute("select content, time from posts order by time desc")
-  return cursor.fetchall()
+  all_posts = cursor.fetchall()
   conn.close()
+  return all_posts
 
 def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
